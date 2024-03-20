16ed2fbf33d24927a7d259e611f232ba7a556a25
BappiGorain <146017226+BappiGorain@users.noreply.github.com>
2024-03-20 17:48:14 +1200

    Add files via upload

--- Class Work/FinallyBlock.java
+++ Class Work/FinallyBlock.java
@@ -1,17 +1,40 @@
 // Finally block execute even if there is some exception
 
+
+import java.util.Scanner;
+
 public class FinallyBlock {
-	public static void main(String[] args) {
-		try {
-		System.out.println(10/0);
+	
+	static Scanner sc = new Scanner(System.in);
+	
+	static int divide(int a, int b)
+	{
+		try
+		{
+			int div = a/b;
+			return div;
+			
 		}
-		catch (Exception e) {
+		catch(ArithmeticException ae)
+		{
+			System.out.println("Denominator cannot be zero");
+			System.out.print("Enter denominator again: ");
+			int c = sc.nextInt();
+			return divide(a,c);
 			
 		}
 		
 		finally
 		{
-			System.out.println("Finally Block");
+			sc.close();
+			System.out.println("Finally block Executed");
 		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(divide(5,0));
+		
 	}
 }
