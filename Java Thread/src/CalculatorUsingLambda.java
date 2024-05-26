77aa0959180121a9a4cadb8bda2763b6e789f0ee
BappiGorain <146017226+BappiGorain@users.noreply.github.com>
2024-05-26 22:57:59 +0530

    Create CalculatorUsingLambda.java

---
+++ Java Thread/src/CalculatorUsingLambda.java
@@ -0,0 +1,40 @@
+interface Calculator
+{
+	double calculate(double a, double b);
+}
+
+class CalculatorUsingLambda
+{
+	public static void main(String args[])
+	{
+		Calculator add = (a,b) ->
+			{
+				return a + b;
+			};
+				
+			
+		Calculator substract = (a,b) ->
+			{
+				return a - b;
+			};
+						
+		Calculator multiplication =(a,b) ->
+			{
+				return a * b;
+			};
+					
+			
+		Calculator divide = (a,b) ->
+			{
+				return a / b;
+			};
+				
+				
+			
+		System.out.println(add.calculate(10,20));
+		System.out.println(substract.calculate(20, 10));
+		System.out.println(multiplication.calculate(10, 20));
+		System.out.println(divide.calculate(20, 10));
+				
+	}
+}
